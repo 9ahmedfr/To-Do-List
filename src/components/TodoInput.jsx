d83c0086a536dd260cb7711a6f8097b63592c77b
9ahmedfr <ahmedfrahuman@gmail.com>
2025-01-15 23:44:38 +0300

    Todo input Handled

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { useState } from "react"
 
-export default function TodoInput() {
+export default function TodoInput(props) {
+
+  const {handleAddTodos} = props
+  const [todoValue, setTodoValue] = useState('')
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleAddTodos(todoValue)
+      setTodoValue('');
+    }
+  }
+
   return (
     <header>
-      <input placeholder='Enter todo...' />
-      <button>Add</button>
+      <input value={todoValue} onChange={(e) => {setTodoValue(e.target.value)}} onKeyDown={handleKeyDown} placeholder='Enter todo...' />
+      <button onClick={() => {
+        handleAddTodos(todoValue)
+        setTodoValue('')
+      }}>Add</button>
     </header>
   )
 }
